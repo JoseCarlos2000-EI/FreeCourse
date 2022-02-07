7ff5b11b3a7b000c4f5136cf162c59022026d8fd
Jose Carlos <josecarlossneider@gmail.com>
2022-02-07 18:18:13 -0500

    Crear metodo API Rest Listar Cursos

--- apps/course/api.py
+++ apps/course/api.py
@@ -7,4 +7,7 @@ class CursoListApiView(ListAPIView):
 
     def get_queryset(self):
         model = self.get_serializer().Meta.model
+        name = self.request.GET.get('name')
+        if name is not None or name.strip() != '':
+            return model.objects.filter(status=True, name__icontains=name)
         return model.objects.filter(status=True)
